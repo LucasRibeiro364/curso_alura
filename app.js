4605f8c696581769b7fa00a17f5970a80c40f938
Lucas RIbeiro <lucasribeiro364@gmail.com>
2025-06-30 23:20:20 -0300

    Jogo do número secreto - 3

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto)
 let chute;
 let tentativas = 1;
 
 //while = enquanto
 while (chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt('Escolha um número entre 1 e 100');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+       break
     } else {
         if (chute > numeroSecreto ) {
             alert(`O número secreto é menor que ${chute}`);
@@ -20,5 +20,13 @@ while (chute != numeroSecreto){
         //tentativas = tentativas + 1;
             tentativas++;
     }
-}
+} 
+// if (tentativas > 1) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// } else {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// }
 
+// OPERADOR TERNÁRIO
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
