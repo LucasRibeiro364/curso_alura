25c6e7f8a1e7c9a98689e788deaaccb3aff60b29
Lucas RIbeiro <lucasribeiro364@gmail.com>
2025-06-30 23:43:10 -0300

    Jogo do número secreto 4

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
-console.log(numeroSecreto)
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
-//while = enquanto
-while (chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 e 100');
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-       break
+        break;
     } else {
-        if (chute > numeroSecreto ) {
+        if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
-        }
-        else {
+        } else {
             alert(`O número secreto é maior que ${chute}`);
         }
-        //tentativas = tentativas + 1;
-            tentativas++;
+        // tentativas = tentativas + 1;
+        tentativas++;
     }
-} 
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
 // if (tentativas > 1) {
 //     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
 // } else {
 //     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
 // }
-
-// OPERADOR TERNÁRIO
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
